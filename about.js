424d36e944a01d788eab78d125e2e046e21130d3
khifo <khifo09@yahoo.com>
2022-12-19 01:37:32 -0700

    part 2 edit javascript

--- about.js
+++ about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+		alert('The form has been submitted successfully') 
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+const imageElement = document.querySelector('img'); // Select the image element
+
+imageElement.addEventListener('mouseover', function() {
+  alert('You have a great taste in pictures!');
+});
