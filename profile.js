58d1ab9e3bd0e406056368c7f83d3fd7fa2982f7
khifo <khifo09@yahoo.com>
2022-12-19 17:47:54 -0700

    part 4

---
+++ profile.js
@@ -0,0 +1,10 @@
+document.getElementById("color").addEventListener("click", function() {
+    alert("My favorite color is blue!");
+  });
+  document.getElementById("place").addEventListener("click", function() {
+    alert("My favorite place is Hawaii!");
+  });
+  document.getElementById("ritual").addEventListener("click", function() {
+    alert("My favorite ritual is a wedding!");
+  });
+       
